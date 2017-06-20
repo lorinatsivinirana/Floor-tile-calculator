716d77d45002b97d429b8d674479624364ed7c2b
lorinatsivinirana <lorina.tnj@gmail.com>
2017-06-20 23:49:15 +0400

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,13 +1,22 @@
 window.onload = function() {
-document.getElementById("btnCheck").addEventListener("click",handleClick);
+
+el1 = document.getElementById('tnumber');
+el2 = document.getElementById('tprice');
 
 function handleClick(){
 var roomsizelength = document.getElementById("rsizel").value;
 var roomsizewidth = document.getElementById("rsizew").value;
-var roomdimension = 0;
-roomdimension = roomsizelength*roomsizewidth;
+var tilesize = document.getElementById("tsize").value;
+var unitprice = document.getElementById("uprice").value;
 
+var roomdimension = roomsizelength*roomsizewidth;
+var tilenumber = roomdimension/tilesize;
+var totalprice = tilenumber*unitprice;
+
+el1.innerHTML = 'Tiles:' + tilenumber;
+el2.innerHTML = 'Price:' + totalprice;
 	//when button is clicked, do stuff here
-window.alert(roomdimension);
+//window.alert(totalprice);
+document.getElementById("btnCheck").addEventListener("click",handleClick);
 };
 }
