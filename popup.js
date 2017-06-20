e4ea781c7db63ebed094137d16c67342c5c685f2
lorinatsivinirana <lorina.tnj@gmail.com>
2017-06-20 21:05:34 +0400

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,8 +1,12 @@
 window.onload = function() {
 document.getElementById("btnCheck").addEventListener("click",handleClick);
+
 function handleClick(){
-	
+var roomsizelength = document.getElementById("rsizel");
+var roomsizewidth = document.getElementById("rsizew");
+var roomdimension = roomsizelength*roomsizewidth;
+
 	//when button is clicked, do stuff here
-//window.alert("button was clicked");
+window.alert("result");
 };
 }
