2607444a870ee8923fd0aad07b8fb0c2d4d1ae3a
lorinatsivinirana <lorina.tnj@gmail.com>
2017-06-20 21:37:21 +0400

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,10 +1,11 @@
 window.onload = function() {
-document.getElementById("btnCheck").addEventListener("click",handleClick);
+document.getElementById("btnCheck").addEventListener("click",handleClick).value;
 
 function handleClick(){
 var roomsizelength = document.getElementById("rsizel");
 var roomsizewidth = document.getElementById("rsizew");
-var roomdimension = roomsizelength*roomsizewidth;
+var roomdimension = 0;
+roomdimension = roomsizelength*roomsizewidth;
 
 	//when button is clicked, do stuff here
 window.alert(roomdimension);
